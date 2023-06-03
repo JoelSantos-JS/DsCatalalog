dcf3801ec90eb626769043eafe94ccc902eaa90a
Joel Santos <joeltere9@gmail.com>
2023-06-03 14:53:20 -0300

    day 03

--- src/test/java/com/devsuperior/DsCatalog/services/ProductServiceTest.java
+++ src/test/java/com/devsuperior/DsCatalog/services/ProductServiceTest.java
@@ -1,16 +1,28 @@
 package com.devsuperior.DsCatalog.services;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.devsuperior.DsCatalog.exception.ResourceExceptionHandler;
+import com.devsuperior.DsCatalog.dto.ProductDTO;
+import com.devsuperior.DsCatalog.model.Product;
 import com.devsuperior.DsCatalog.repository.ProductRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -25,6 +37,8 @@ public class ProductServiceTest {
     private long existingId;
 
     private long nonExistingId;
+
+    private Product product = new Product("Joel", "Jose", 20.0, "imag", Instant.now());
 
     @Test
 
@@ -43,6 +57,17 @@ public class ProductServiceTest {
     public void deleteShouldDeleteObjectWhenIdExists() {
         Assertions.assertDoesNotThrow(() -> service.delete(existingId));
         Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        Assertions.assertNotNull(result);
+        verify(repository, Mockito.times(1)).findAll(pageable);
+
     }
 
 }
