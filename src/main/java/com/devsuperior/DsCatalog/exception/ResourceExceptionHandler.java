4ba2360280187520d399b8ae0f7443b8e40f722b
Joel Santos <joeltere9@gmail.com>
2023-06-05 16:20:27 -0300

    finish tratament error

--- src/main/java/com/devsuperior/DsCatalog/exception/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/DsCatalog/exception/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,14 +27,18 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
-        StandardError err = new StandardError();
+    public ResponseEntity<ValidationErro> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationErro err = new ValidationErro();
         err.setTimestamp(Instant.now());
         err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
         err.setError("Resource not found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
 
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+
+        }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
