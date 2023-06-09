28a9334e43c1810084631ef46d3865c3feaf5bc1
Joel Santos <joeltere9@gmail.com>
2023-06-09 10:55:24 -0300

    resource server

--- src/main/java/com/devsuperior/DsCatalog/services/DbService.java
+++ src/main/java/com/devsuperior/DsCatalog/services/DbService.java
@@ -34,30 +34,18 @@ public class DbService {
     public List<Category> instaciaDados() {
         List<Category> list = new ArrayList<>();
         List<Product> products = new ArrayList<>();
-        List<Role> roles = new ArrayList<>();
-        List<User> users = new ArrayList<>();
 
         list.add(new Category(1L, "Books"));
         list.add(new Category(2L, "Electronics"));
         list.add(new Category(3L, "Computers"));
-
-        roles.add(new Role(1L, "ROLE_ADMIN"));
-        roles.add(new Role(2L, "ROLE_USER"));
-        roles.add(new Role(3L, "ROLE_MANAGER"));
-        roles.add(new Role(4L, "ROLE_SUPERVISOR"));
-
-        users.add(new User(1L, "Joel", "Jose", "joeltere@gmail.com", "joel"));
-        users.add(new User(2L, "Maria", "Jose", "maria@gmail.com", "maria"));
 
         products.add(new Product(1L, "Macbook Pro", "Mack", 1200.0, "jjjk", Instant.now()));
         products.add(new Product(2L, "Notebbok Pro", "Mack", 1200.0, "jjjk", Instant.now()));
         products.add(new Product(3L, "Face Pro", "Mack", 1200.0, "jjjk", Instant.now()));
         products.add(new Product(4L, "Jogos Pro", "Mack", 1200.0, "jjjk", Instant.now()));
 
-        rr.saveAll(roles);
         pr.saveAll(products);
         cr.saveAll(list);
-        ur.saveAll(users);
 
         return list;
 
